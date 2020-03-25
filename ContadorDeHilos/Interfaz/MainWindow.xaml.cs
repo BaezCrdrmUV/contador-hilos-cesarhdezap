4fb6843eb87b684feaf848cc6c6f970f2490847d
CESAR CHRISTOPHER HERNANDEZ APARICIO <zS17012947@estudiantes.uv.mx>
2020-03-24 23:37:30 -0600

    Se añaden el contador de hilo automatico y manual en la interfaz gráfica.

---
+++ ContadorDeHilos/Interfaz/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interfaz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int ContadorManual = 0;
+        public int ContadorAutomatico = 0;
+        Thread HiloContadorManual;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonContadorManual_Click(object sender, RoutedEventArgs e)
+        {
+            ContadorManual++;
+            LabelContadorManual.Content = ContadorManual.ToString();
+        }
+
+        private void ButtonContadorAutomatico_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)CheckboxUtilizarHilos.IsChecked)
+            {
+                HiloContadorManual = new Thread(new ThreadStart(HiloAutomatico));
+                HiloContadorManual.Start();
+            }
+            else
+            {
+                while (!(bool)CheckboxUtilizarHilos.IsChecked)
+                {
+                    ContadorAutomatico++;
+                    LabelContadorAutomatico.Content = ContadorAutomatico.ToString();
+                }
+            }
+        }
+
+        private void CheckboxUtilizarHilos_Checked(object sender, RoutedEventArgs e)
+        {
+            if (HiloContadorManual != null)
+            {
+                if (HiloContadorManual.IsAlive && !(bool)CheckboxUtilizarHilos.IsChecked)
+                {
+                    HiloContadorManual.Abort();
+                }
+            }
+        }
+
+        public void HiloAutomatico()
+        {
+            try
+            {
+                
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    LabelContadorAutomatico.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() => this.AumentarContadorAutomatico()));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+        }
+
+        public void AumentarContadorAutomatico()
+        {
+            ContadorAutomatico++;
+            LabelContadorAutomatico.Content = ContadorAutomatico.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+#Contador de hilos
+Alex, [20.03.20 16:36]
+en la segundo tienes que hacer una aplicación gráfica que teng dos contadores, uno automatico y una al que le des click a un botón para que suba
+
+Alex, [20.03.20 16:36]
+y tiene que haber un checkbox que diga si la aplicación deberia usar hilos o no
+
+Alex, [20.03.20 16:37]
+si le das que no al iniciar el automático, se bloquea la UI
+
+Si le das si a hilos no se bloquea
